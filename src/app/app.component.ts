1f879cf3efd9f88cd8db757f053fe6eab5cb4d7d
Hryhorii <grishachapliy1@gmail.com>
2020-09-25 09:59:33 +0300

    app component and navigation bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {EmitterService} from './services/emitter.service';
+import {User} from './model/user';
+import {AuthService} from './services/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,54 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'course-enrollment-client';
+  searchText: string;
+  currentUser: User;
+  private _serviceSubscription;
+
+  constructor(private authService: AuthService, private router: Router, private emitterService: EmitterService) {
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    this._serviceSubscription = this.emitterService.onLogin.subscribe({
+      next: (event: any) => {
+        this.currentUser = JSON.parse(event.message);
+      }
+    });
+  }
+
+  logOut() {
+    this._serviceSubscription.unsubscribe();
+    this.authService.logOut()
+      .subscribe(
+        data => {
+          this.currentUser = null;
+          this.router.navigate(['/login']);
+        },
+        error => {
+
+        });
+  }
+
+  search() {
+    if (this.router.url == '/course-list') {
+      this.emitterService.emitSearch(0, this.searchText);
+      return;
+    }
+    this.router.navigate(['/course-list', {id: 0, text: this.searchText}]);
+    console.log(this.router.url);
+  }
+
+  searchAll() {
+    if (this.router.url == '/course-list') {
+      this.emitterService.emitSearch(1, this.searchText);
+      return;
+    }
+    this.router.navigate(['/course-list', {id: 1}]);
+  }
+
+  searchPopular() {
+    if (this.router.url == '/course-list') {
+      this.emitterService.emitSearch(2, this.searchText);
+      return;
+    }
+    this.router.navigate(['/course-list', {id: 2}]);
+  }
 }
--- src/app/services/emitter.service.ts
+++ src/app/services/emitter.service.ts
@@ -1,18 +1,17 @@
-import { Injectable } from '@angular/core';
-import { EventEmitter } from '@angular/core';
+import {Injectable, EventEmitter} from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmitterService {
-    public onLogin: EventEmitter<any> = new EventEmitter<any>();
-    public onSearch: EventEmitter<any> = new EventEmitter<any>();
+  public onLogin: EventEmitter<any> = new EventEmitter<any>();
+  public onSearch: EventEmitter<any> = new EventEmitter<any>();
 
-    public emitLogin(message: string) {
-        this.onLogin.emit({message: message});
-    }
+  public emitLogin(message: string) {
+    this.onLogin.emit({message});
+  }
 
-    public emitSearch(id: any, text: string) {
-        this.onSearch.emit({id: id, text: text});
-    }
+  public emitSearch(id: any, text: string) {
+    this.onSearch.emit({id, text});
+  }
 }
